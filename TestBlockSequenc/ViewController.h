aa213aa4ee717648224c4dda270a673ad3370669
Herve Peroteau <herve.peroteau@gmail.com>
2013-08-14 07:02:04 +0200

    Initial Commit

---
+++ TestBlockSequenc/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestBlockSequenc
+//
+//  Created by Hervé PEROTEAU on 14/08/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TestBlockSequencTests/TestBlockSequencTests.m
@@ -0,0 +1,34 @@
+//
+//  TestBlockSequencTests.m
+//  TestBlockSequencTests
+//
+//  Created by Hervé PEROTEAU on 14/08/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestBlockSequencTests : XCTestCase
+
+@end
+
+@implementation TestBlockSequencTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
